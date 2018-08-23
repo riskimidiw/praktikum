8e5974ff95d00b91c0a8390e5f534412bd14bfca
riskimidiw <riskimidiw@gmail.com>
2018-08-24 02:08:42 +0700

    Add pertemuan-1/latihan solution

---
+++ algoritma-1/pertemuan-1/latihan/01-luas-segitiga.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    double alas = 5;
+    double tinggi = 7;
+    double luasSegitiga = alas * tinggi / 2;
+
+    printf("%.2f\n", luasSegitiga);
+}
---
+++ algoritma-1/pertemuan-1/latihan/02-bebek.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 15;
+    int m = 3;
+
+    cout << "masing-masing " << n/m << endl;
+    cout << "bersisa " << n%m << endl;
+
+    return 0;
+}
