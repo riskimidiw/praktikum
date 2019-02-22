bd9be22c5c2aa160785fed68ad7c812834e93446
riskimidiw <riskimidiw@gmail.com>
2019-02-22 13:01:21 +0700

    add struct.cpp

--- algoritma-2/pertemuan-2(struct)/struct.cpp
+++ algoritma-2/pertemuan-2(struct)/struct.cpp
@@ -2,16 +2,61 @@
 using namespace std;
 
 typedef struct {
-    string nama;
-} Mahasiswa;
+    string nama, keterangan;
+    int jumlah;
+    double harga;
+}Barang;
 
-void ubahNama(Mahasiswa *mhs) {
-    (*mhs).nama = "ubah";
-}
+typedef struct {
+    string nama, alamat, noHp;
+    int jumlahBarang;
+    Barang barang[100];
+}Penjual;
 
 int main() {
-    Mahasiswa mhs = {"nama panjang"};
-    cout << "sebelum : " << mhs.nama << endl;
-    ubahNama(&mhs);
-    cout << "sesudah : " << mhs.nama << endl;
+    int jmlPenjual;
+    cout << "Jumlah Penjual: ";
+    cin >> jmlPenjual;
+    Penjual arrPenjual[jmlPenjual];
+
+    for(int i=0; i<jmlPenjual; i++) {
+        cin.ignore();
+        cout << "Nama          : ";
+        getline(cin, arrPenjual[i].nama);
+        cout << "Alamat        : ";
+        getline(cin, arrPenjual[i].alamat);
+        cout << "No HP         : ";
+        getline(cin, arrPenjual[i].noHp);
+        cout << "Jumlah Barang : ";
+        cin >> arrPenjual[i].jumlahBarang;
+
+        for(int j=0; j<arrPenjual[i].jumlahBarang; j++) {
+            cin.ignore();
+            cout << "\tNama       : ";
+            getline(cin, arrPenjual[i].barang[j].nama);
+            cout << "\tKeterangan : ";
+            getline(cin, arrPenjual[i].barang[j].keterangan);
+            cout << "\tJumlah     : ";
+            cin >> arrPenjual[i].barang[j].jumlah;
+            cout << "\tHarga      : ";
+            cin >> arrPenjual[i].barang[j].harga;
+            cout << endl;
+        }
+    }
+
+    cout << "\n===========================================\n\n";
+
+    for(int i=0; i<jmlPenjual; i++) {
+        cout << "Nama          : " << arrPenjual[i].nama << endl;
+        cout << "Alamat        : " << arrPenjual[i].alamat << endl;
+        cout << "No HP         : " << arrPenjual[i].noHp << endl;
+        cout << "Jumlah Barang : " << arrPenjual[i].jumlahBarang << endl;
+
+        for(int j=0; j<arrPenjual[i].jumlahBarang; j++) {
+            cout << "\tNama       : " << arrPenjual[i].barang[j].nama << endl;
+            cout << "\tKeterangan : " << arrPenjual[i].barang[j].keterangan << endl;
+            cout << "\tJumlah     : " << arrPenjual[i].barang[j].jumlah << endl;
+            cout << "\tHarga      : " << arrPenjual[i].barang[j].harga << endl << endl;
+        }
+    }
 }
